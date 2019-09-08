f3cb2966abe6b9b82c9942df87ff325ab33db025
JelenaVucetic <jelenavucetic24@gmail.com>
2019-09-08 16:23:53 -0700

    first page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import classes from './Layout.css';
 
+import Logo from '../../assets/images/logo.png';
+
 const layout = (props) => (
 
     <div>
-        <div className={classes.Header}> Header, navigation</div>
+        <div className={classes.Header}>
+            <div className={classes.Nav}>
+                <img src={Logo} alt='img' />
+                <div className={classes.NavItems}>
+                    <a href='#'>Pocetna </a>
+                    <a href='#'>O nama </a>
+                    <a href='#'>Usluge </a>
+                    <a href='#'>Radovi </a>
+                    <a href='#'>Kontakt </a>
+
+                </div>
+            </div>
+            <h1> Enterijer i namjestaj po mjeri</h1>
+        </div>
+        <div className={classes.Ruler1} ></div>
+        <div className={classes.Ruler2} ></div>
+
         <main>
             {props.children}
         </main>
--- src/containers/Partido/Partido.js
+++ src/containers/Partido/Partido.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import classes from './Partido.css';
+import Video from '../../assets/videos/video.mp4'
 class Partido extends Component {
     render() {
         return (
             <div>
-                <div>video</div>
+                <div className={classes.Video}>
+                    <video id="background-video" >
+                        <source src={Video} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
                 <div>o nama</div>
                 <div>radovi</div>
                 <div>kontakt</div>
