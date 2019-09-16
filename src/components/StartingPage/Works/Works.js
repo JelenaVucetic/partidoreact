bc01c806a3d2c721fc19cd7c42897b5b873e2467
JelenaVucetic <jelenavucetic24@gmail.com>
2019-09-16 05:37:50 -0700

    pocetna radovi

--- src/components/StartingPage/Works/Works.js
+++ src/components/StartingPage/Works/Works.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Works.css';
 import Image1 from '../../../assets/images/radovi1.png';
@@ -6,17 +6,92 @@ import Image2 from '../../../assets/images/radovi2.png';
 import Image3 from '../../../assets/images/radovi3.png';
 import Image4 from '../../../assets/images/radovi4.png';
 
-const works = () => (
-    <div className={classes.Radovi}>
-        <h1>Radovi</h1>
-        <div className={classes.Images}>
-            <div > <img src={Image1} alt='img' /> </div>
-            <div> <img src={Image3} alt='img' /> </div>
-            <div> <img src={Image2} alt='img' /> </div>
-            <div> <img src={Image4} alt='img' /> </div>
+class Works extends Component {
 
-        </div>
-    </div>
-);
+    state = { isOpen: false };
 
-export default works;
+    showImageHeandler = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+        console.log('clicked');
+    }
+
+
+    render()  {
+        return (
+            <div className={classes.Radovi} >
+                <h1>Radovi</h1>
+                <div className={classes.Images}>
+                    <div > <img src={Image1} alt='img' onClick={() => this.showImageHeandler()} /> </div>
+                    {this.state.isOpen && (
+                        <dialog
+                            className="dialog"
+                            style={{ position: "absolute" }}
+                            open
+                            onClick={this.showImageHeandler}
+                        >
+                            <img
+                                src={Image1}
+                                className={classes.DialogImg}
+                                onClick={this.showImageHeandler}
+                                alt="no image"
+                            />
+                        </dialog>
+                    )}
+                    <div> <img src={Image3} alt='img' onClick={() => this.showImageHeandler()} /> </div>
+                    {this.state.isOpen && (
+                        <dialog
+                            className="dialog"
+                            style={{ position: "absolute" }}
+                            open
+                            onClick={this.showImageHeandler}
+                        >
+                            <img
+                                src={Image2}
+                                className={classes.DialogImg}
+                                onClick={this.showImageHeandler}
+                                alt="no image"
+                            />
+                        </dialog>
+                    )}
+                        <div> <img src={Image2} alt='img' onClick={() => this.showImageHeandler()} /> </div>
+                        {this.state.isOpen && (
+                        <dialog
+                        className="dialog"
+                        style={{ position: "absolute" }}
+                        open
+                        onClick={this.showImageHeandler}
+                    >
+                        <img
+                            src={Image3}
+                            className={classes.DialogImg}
+                            onClick={this.showImageHeandler}
+                            alt="no image"
+                        />
+                        </dialog>
+                    )}
+                    <div> <img src={Image4} alt='img' onClick={() => this.showImageHeandler()} /> </div>
+                    {this.state.isOpen && (
+                        <dialog
+                            className="dialog"
+                            style={{ position: "absolute" }}
+                            open
+                            onClick={this.showImageHeandler}
+                        >
+                            <img
+                                src={Image4}
+                                className={classes.DialogImg}
+                                onClick={this.showImageHeandler}
+                                alt="no image"
+                            />
+                        </dialog>
+                    )}
+
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+export default Works;
